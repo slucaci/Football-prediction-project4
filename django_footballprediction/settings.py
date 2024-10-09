fb3d8221672f02605dcccc241a3973a0fa7bb42f
slucaci <serjlucaci@gmail.com>
2024-10-09 08:08:49 +0100

    Added an Event Model

--- django_footballprediction/settings.py
+++ django_footballprediction/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^&84dqd_wu!h4t64))t92s6#3ay9o&@l0m@rqt=4#vdwc110oa'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -89,6 +89,11 @@ WSGI_APPLICATION = 'django_footballprediction.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- predictions/models.py
+++ predictions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
+class Event(models.Model):
+    """
+    This model represents a football event or match.
+    """
+    event_name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    description = models.TextField(blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="created_events")
+    created_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
