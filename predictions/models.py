93aa5fa3e26e88489e260419eb13e4883814f390
slucaci <serjlucaci@gmail.com>
2024-10-11 17:56:41 +0100

    enable serving of image files using Cloudinary. Each event will have an image stored

--- predictions/models.py
+++ predictions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -14,6 +15,7 @@ class Event(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     location = models.CharField(max_length=200)
     date = models.DateTimeField()
+    featured_image = CloudinaryField('image', default='placeholder')
     description = models.TextField(blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name="created_events")
     created_on = models.DateTimeField(auto_now=True)
