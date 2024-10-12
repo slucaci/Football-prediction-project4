a33a30bb899922aac561ac76897fb8b753dd15f8
slucaci <serjlucaci@gmail.com>
2024-10-12 10:28:50 +0100

    Added content to about page and changed the view so all items can be displayed

--- about/views.py
+++ about/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import About
 
 # Create your views here.
+
 def about_view(request):
-    about_info = About.objects.first()
+    about_info = About.objects.all() 
     return render(request, 'about/about.html', {'about_info': about_info})
